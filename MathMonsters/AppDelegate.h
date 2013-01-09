92ff73278c453ebaf689d1cd56d10c96230cbef6
Ellen Shapiro <designatednerd@gmail.com>
2013-01-08 19:14:42 -0600

    Initial Commit

---
+++ MathMonsters/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MathMonsters
+//
+//  Created by Transferred on 1/8/13.
+//  Copyright (c) 2013 Designated Nerd Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
